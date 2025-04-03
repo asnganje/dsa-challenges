6fb1308e0c1a7ab50f4b77a3e6a39dd1e2492793
asnganje <abd.nganje@gmail.com>
2025-04-03 12:00:41 +0300

    Added a controller method for reversing words

---
+++ Rails/reverse_words.rb
@@ -0,0 +1,18 @@
+# Question: Reverse Words in a String
+# Write a Rails controller action that takes a string as input and returns the string with the words reversed.
+
+class Controller
+  def reverse_str(str)
+    return false if str.size == 0
+    return str if str.size == 1
+    splitted = str.downcase.split(' ')
+    result=[]
+    (splitted.size-1).downto(0) do |i|
+      result << splitted[i]
+    end
+    result.join(' ')
+  end
+end
+
+myClass = Controller.new
+print myClass.reverse_str("Hello world from Rails")
