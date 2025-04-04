326a5cdcace40a234a616826e0f8c0dd8300a481
asnganje <abd.nganje@gmail.com>
2025-04-04 14:30:43 +0300

    add prblem to fib soln

--- Pure ruby/fibonacci.rb
+++ Pure ruby/fibonacci.rb
@@ -1,3 +1,10 @@
+### **3. Fibonacci Sequence (Memoized)**
+# - **Problem**: Write a function that computes the nth number in the Fibonacci sequence using dynamic programming (memoization).
+# - **Example**:  
+#   Input: `6`  
+#   Output: `8`  
+#   (Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8…)
+
 def fib(n)
   return n if(n < 2) 
   fib(n-1)+fib(n-2)
