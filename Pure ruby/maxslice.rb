cde38e23f26f4dcc3e3c36210833d49439051267
asnganje <abd.nganje@gmail.com>
2025-04-12 07:55:05 +0300

    max sub array sum solution

---
+++ Pure ruby/maxslice.rb
@@ -0,0 +1,15 @@
+def soln(arr)
+  return nil if arr.empty?
+  return nil if arr.size == 1 && arr[0].class != Integer
+  return arr[0] if arr.size == 1 && arr[0].class == Integer
+  max_slice_sum = arr[0] #store sum of the max slice
+  max_end_sum = arr[0] # store sum of the temp slice as move across arr
+
+  arr[1..-1].each do |val|
+    max_end_sum = [val, val + max_end_sum].max
+    max_slice_sum = [max_slice_sum, max_end_sum].max
+  end
+  max_slice_sum
+end
+
+p soln([3,-2,5,-1])
